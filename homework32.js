b43277f3629f395f39a85a4f764d72a8b7edbb3f
pasha1996 <pasha90@i.ua>
2023-08-19 18:19:57 +0300

    1908 1819

---
+++ homework32.js
@@ -0,0 +1,23 @@
+let question = prompt(`що ви хочете зробити?(+,-,*,/)`, `зробіть вибір`);
+let number1 = prompt(`перше число`, `введіть перше число`);
+let number2 = prompt(`друге число`, `введіть друге число`);
+number1 = Number(number1);
+number2 = Number(number2);
+let result = 0;
+switch (question) {
+    case `+`:
+        result = number1 + number2;
+        break;
+    case `-`:
+        result = number1 - number2;
+        break;
+    case `*`:
+        result = number1 * number2;
+        break;
+    case `/`:
+        result = number1 / number2;
+        break;
+default:
+    break;
+}
+alert(`${number1}${question}${number2}=${result}`);
