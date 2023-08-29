c00fbc095b76140ccc774f497435967d081b9bc4
pasha1996 <pasha90@i.ua>
2023-08-29 16:03:09 +0300

    this is it

---
+++ hw51.js
@@ -0,0 +1,14 @@
+const length = parseInt(prompt("Введите длину массива:"));
+const array = [];
+
+for (let i = 0; i < length; i++) {
+    const element = parseInt(prompt(`Введите элемент ${i + 1}:`));
+    array.push(element);
+}
+console.log("Начальный массив:", array);
+
+array.sort((a, b) => a - b);
+console.log("Отсортированный массив:", array);
+
+array.splice(1, 4);
+console.log("Массив после удаления элементов:", array);
