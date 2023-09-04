dd488287e3609df782f9a1007025a56fc25a6ca0
pasha1996 <pasha90@i.ua>
2023-09-04 20:43:09 +0300

    add homew 71-2

--- hw72.js
+++ hw72.js
@@ -3,11 +3,8 @@ function power(num, degree) {
         return 1;
     }
     else {
-        console.log(`power(${num}, ${degree}) = ${num} * power(${num}, ${degree - 1})`);
-        const result = num * power(num, degree - 1);
-        console.log(`power(${num}, ${degree}) = ${result}`);
-        return result;
+        return num * power(num, degree - 1);
     }
 }
 const result = power(2, 3);
-console.log(`Результат: ${result}`);
+console.log(result);
